61736d3e5c938c6c58f4ef189382935d37ca49d4
KaluGN <kaluguzmann@gmail.com>
2018-07-13 08:17:53 -0500

    funcioando ordenado de percent con algunos errores de computer

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+//////AQUI VA LO DE DATA
 window.computeUsersStats = (users, progress, courses) => {
   // console.log(users,progress,courses);
   let recorrerUserStats = users.filter(elementUser => elementUser.role === 'student');
@@ -33,7 +34,6 @@ window.computeUsersStats = (users, progress, courses) => {
                   if (exercises[exercise].completed === 1) {
                     contadorCompleted++
                   }
-
                 }
               }
             }
@@ -127,25 +127,35 @@ window.computeUsersStats = (users, progress, courses) => {
   return recorrerUserStats;
 };
 
-
-
 window.sortUsers = (users, orderBy, orderDirection) => {
-  const orderName = users.sort(function (a,b){
+ 
+  
+  const orderName = users.sort(function (a, b) {
     var x = a.name.toUpperCase();
     var y = b.name.toUpperCase();
     if (x > y) { return 1; }
-    if (x < y) { return -1;}
+    if (x < y) { return -1; }
     return 0;
   });
-  if (orderBy === 'name' && orderDirection === 'asc'){
-    console.log(orderName)
+  if (orderBy === 'name' && orderDirection === 'asc') {
+    // console.log(orderName)
     return orderName
-  }else if (orderBy === 'name' && orderDirection === 'dsc') {
+  } else if (orderBy === 'name' && orderDirection === 'dsc') {
     const nuevo = orderName.reverse();
     return nuevo
-      }
+  }else if (orderBy === 'percent' & orderDirection === 'asc') {
+    const orderNew = users.sort(function (a, b) {
+      return a.stats.percent - b.stats.percent
+    });
+    return orderNew;
+  } else if (orderBy === 'percent' & orderDirection === 'dsc') {
+    const orderNew = users.sort(function (a, b) {
+      return b.stats.percent - a.stats.percent
+    });
+    return orderNew;
+  }
   
-  
+
   // return users.sort(); 
 }
 
@@ -158,19 +168,198 @@ window.filterUsers = (users, search) => {
 }
 
 
-
-
-
 window.processCohortData = (options) => {
+  console.log(options);
+  
   const courses = Object.keys(options.cohort.coursesIndex);
   // console.log(courses)
   let estudents = computeUsersStats(options.cohortData.users, options.cohortData.progress, courses);
+  console.log(estudents);
+  
   estudents = sortUsers(estudents, options.orderBy, options.orderDirection);
+  console.log(estudents);
+  
   if (options.search !== '') {
     estudents = filterUsers(estudents, options.search)
   }
-
-
+  console.log(estudents);
+  
   return estudents;
 }
 
+
+// window.computeUsersStats = (users, progress, courses) => {
+//   // console.log(users,progress,courses);
+//   let recorrerUserStats = users.filter(elementUser => elementUser.role === 'student');
+//   //console.log(recorrerUserStats);
+//   const percentUsers = (recorrerUserStats) => {
+//     let contador = 0;
+//     courses.forEach(course1 => {
+
+//       const progressUsers = progress[recorrerUserStats.id];
+//       if (progressUsers && Object.keys(progressUsers).length > 0 && !Array.isArray(progressUsers)) {
+//         contador += progressUsers[course1].percent;
+//       }
+//     });
+//     return (contador / courses.length)
+//   }
+
+//   //Función para obtener progreso y demás datos de 'exercises'
+//   const objectExercise = (recorrerUserStats) => {
+//     let contadorTotal = 0;
+//     let contadorCompleted = 0
+//     courses.forEach(course1 => {
+//       const progressUsers = progress[recorrerUserStats.id];
+//       if (progressUsers && Object.keys(progressUsers).length > 0 && !Array.isArray(progressUsers)) {
+//         const units = Object.values(progressUsers[course1].units)
+//         units.forEach(partsUnits => {
+//           const parts = Object.values(partsUnits.parts)
+//           for (let part in parts) {
+//             for (let element in parts[part]) {
+//               if (element === "exercises") {
+//                 const exercises = (parts[part][element]);
+//                 for (exercise in exercises) {
+//                   contadorTotal++
+//                   if (exercises[exercise].completed === 1) {
+//                     contadorCompleted++
+//                   }
+
+//                 }
+//               }
+//             }
+//           }
+//         });
+//       }
+//     });
+//     //creación del objeto exercises para mostrar en función global
+//     const exercises = new Object();
+//     exercises.total = contadorTotal,
+//       exercises.completed = contadorCompleted
+//     exercises.percent = contadorCompleted * 100 / contadorTotal
+//     return exercises
+//   }
+//   const objectReads = (recorrerUserStats) => {
+//     let contadorTotal = 0;
+//     let contadorCompleted = 0
+//     courses.forEach(course1 => {
+//       const progressUsers = progress[recorrerUserStats.id];
+//       if (progressUsers && Object.keys(progressUsers).length > 0 && !Array.isArray(progressUsers)) {
+//         const units = Object.values(progressUsers[course1].units)
+//         units.forEach(partsUnits => {
+//           const parts = Object.values(partsUnits.parts)
+//           for (let part in parts) {
+//             // console.log(parts[part]['type']);
+//             if (parts[part]['type'] === "read") {
+//               contadorTotal++
+//               if (parts[part]['completed'] === 1) {
+//                 contadorCompleted++
+//               }
+//             }
+//           }
+//         });
+//       }
+//     });
+//     const read = new Object();
+//     read.total = contadorTotal,
+//       read.completed = contadorCompleted
+//     read.percent = Math.round(contadorCompleted * 100 / contadorTotal)
+//     return read
+//   }
+//   const objectQuizz = (recorrerUserStats) => {
+//     let contadorTotal = 0;
+//     let contadorCompleted = 0;
+//     let sumaScore = 0;
+//     courses.forEach(course1 => {
+//       const progressUsers = progress[recorrerUserStats.id];
+//       if (progressUsers && Object.keys(progressUsers).length > 0 && !Array.isArray(progressUsers)) {
+//         const units = Object.values(progressUsers[course1].units)
+//         units.forEach(partsUnits => {
+//           const parts = Object.values(partsUnits.parts)
+//           for (let part in parts) {
+//             // console.log(parts[part]['type']);
+//             if (parts[part]['type'] === "quiz") {
+//               contadorTotal++
+//               if (parts[part]['completed'] === 1) {
+//                 contadorCompleted++
+//               }
+//               if (parts[part].hasOwnProperty('score')) {
+//                 sumaScore += parts[part].score;
+//               }
+//             }
+//           }
+//         });
+//       }
+//     });
+//     const quiz = new Object();
+//     quiz.total = contadorTotal,
+//       quiz.completed = contadorCompleted,
+//       quiz.percent = Math.round(contadorCompleted * 100 / contadorTotal),
+//       quiz.scoreSum = sumaScore,
+//       quiz.scoreAvg = Math.round(sumaScore / contadorCompleted)
+//     return quiz
+//   }
+
+//   recorrerUserStats = recorrerUserStats.map(objectUser => {
+//     const userWithStats = {
+//       name: objectUser.name,
+//       stats: {
+//         percent: percentUsers(objectUser),
+//         exercises: objectExercise(objectUser),
+//         reads: objectReads(objectUser),
+//         quiz: objectQuizz(objectUser)
+//       }
+//     }
+//     //console.log(userWithStats);
+//     return userWithStats;
+//   })
+//   // console.log(recorrerUserStats);
+
+//   return recorrerUserStats;
+// };
+
+
+
+// window.sortUsers = (users, orderBy, orderDirection) => {
+//   const orderName = users.sort(function (a,b){
+//     var x = a.name.toUpperCase();
+//     var y = b.name.toUpperCase();
+//     if (x > y) { return 1; }
+//     if (x < y) { return -1;}
+//     return 0;
+//   });
+//   if (orderBy === 'name' && orderDirection === 'asc'){
+//     return orderName
+//   }else if (orderBy === 'name' && orderDirection === 'dsc') {
+//     const nuevo = orderName.reverse();
+//     return nuevo
+//   } 
+
+
+
+
+
+//   // return users.sort(); 
+// }
+
+
+// window.filterUsers = (users, search) => {
+//   let userFilter = users.filter(user => {
+//     return user.name.toUpperCase().indexOf(search.toUpperCase()) !== -1;
+//   });
+//   return userFilter;
+// }
+
+
+
+// window.processCohortData = (options) => {
+//   const courses = Object.keys(options.cohort.coursesIndex);
+//   // console.log(courses)
+//   let estudents = computeUsersStats(options.cohortData.users, options.cohortData.progress, courses);
+//   estudents = sortUsers(estudents, options.orderBy, options.orderDirection);
+//   if (options.search !== '') {
+//     estudents = filterUsers(estudents, options.search)
+//   }
+
+
+//   return estudents;
+// }
--- src/main.js
+++ src/main.js
@@ -27,14 +27,14 @@ const llamadoError = () => {
 }
 
 
-//OBJETO PARA FUNCIÓN 4 -- ARGUMENTOS OBJETO GLOBAL
+//ARGUMENTOS OBJETO GLOBAL
 let options = {
   cohort: [],
   cohortData: {
     users: null,
     progress: null,
   },
-  orderBy: 'name', //modificar desde sort (f3)
+  orderBy: 'name', 
   orderDirection: 'asc',
   search: '',
 }
@@ -55,17 +55,17 @@ const viewUser = (nuevoUsers) => {
 }
 
 //MOSTRAR A LOS USERs
-const showUser = (users) => {
   cohortSelect.addEventListener('change', (event) => {
     //console.log(event.target.value)
     const cohort = options.cohorts.find(c => c.id === event.target.value);
     options.cohort = cohort;
     //console.log(options);
     const nuevoUsers = processCohortData(options);
-        
-    viewUser(nuevoUsers)
+    console.log(nuevoUsers);
+    addUsers.innerHTML = ' '    
+   viewUser(nuevoUsers)
   });
-}
+
 
 
 //FUNCIONES PARA LLAMAR A USER - PROGRESS - COHORTS
@@ -74,7 +74,7 @@ const llamadoUser = (event) => {
   const dataUser = JSON.parse(event.target.responseText);
   //console.log(dataUser);
   options.cohortData.users = dataUser;
-  showUser(dataUser);
+
 }
   //console.log(options);
 
@@ -130,13 +130,147 @@ searchStudent.addEventListener('keyup', function () {
   
 })
 
+// console.log(event.target.value);
+  // console.log(selectOrderBy.value);
+
 //LLAMANDO A SORT
 orderButton.addEventListener('click', (event) => {
-  // console.log(event.target.textContent);
-  // console.log(selectOrderBy.value);
   options.orderBy = selectOrderBy.value;
   options.orderDirection = selectOrderTo.value; 
+  console.log(options);
+  
   const newOrder = processCohortData(options);
   addUsers.innerHTML = '';
   viewUser(newOrder);
 })
+
+// //VARIABLES PARA NOMBRAR LA DATA
+// const urlUsers = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
+// const urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+// const urlCohorts = "../data/cohorts.json";
+
+// //llamar los elementos del DOM - HTML
+// let sedes = document.getElementById('sede');
+// let cohortSelect = document.getElementById('cohort');
+// let addUsers = document.getElementById('agregar-alumnas');
+// const searchStudent = document.getElementById('buscar-alumna');
+// const selectOrderBy = document.getElementById('select-ordenar');
+// const selectOrderTo = document.getElementById('mayor-menor');
+// const orderButton = document.getElementById('boton-ordenar');
+
+
+// //ARGUMENTOS OBJETO GLOBAL
+// let options = {
+//   cohort: [],
+//   cohortData: {
+//     users: null,
+//     progress: null,
+//   },
+//   orderBy: 'name', 
+//   orderDirection: 'ASC',
+//   search: '',
+// }
+
+
+// //Funcion que me perimite hacer la petición de la data que está en mi archivo json
+// const getData = (url, callback) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', url);
+//   xhr.onload = callback;
+//   xhr.onerror = llamadoError;
+//   xhr.send();
+// };
+
+// const llamadoError = () => {
+//   console.log('se produjo un error')
+// }
+
+// //MOSTRAR A LOS USERs
+// const showUser = (users) => {
+//   cohortSelect.addEventListener('change', (event) => {
+//     //console.log(event.target.value)
+//     const cohort = options.cohorts.find(c => c.id === event.target.value);
+//     options.cohort = cohort;
+//     //console.log(options);
+//     const nuevoUsers = processCohortData(options);
+//     const viewUser = (nuevoUsers) => {
+//       nuevoUsers.forEach(elementUser => {
+//         addUsers.innerHTML += `<p id="add-students" class="student-data">${
+//           elementUser.name + '\n' +
+//           'Progreso general: ' + elementUser.stats.percent + '\n' +
+//           '% de ejercicios: ' + elementUser.stats.exercises.percent + '\n' +
+//           '% de lecturas: ' + elementUser.stats.reads.percent + '\n' +
+//           '% de quiz: ' + elementUser.stats.quiz.percent
+//           }</p>`;
+//       });
+//     }
+//   });
+// }
+
+// //FUNCIONES PARA LLAMAR A USER - PROGRESS - COHORTS
+// const llamadoUser = (event) => {
+//   //console.log(event.target.responseText);
+//   const dataUser = JSON.parse(event.target.responseText);
+//   //console.log(dataUser);
+//   options.cohortData.users = dataUser;
+//   showUser(dataUser);
+// }
+// //console.log(options);
+// const llamadoProgress = () => {
+//   const dataProgress = JSON.parse(event.target.responseText);
+//   //console.log(dataProgress);
+//   options.cohortData.progress = dataProgress;
+//   //console.log(options);
+
+//   const llamadoCohorts = () => {
+//     const dataCohorts = JSON.parse(event.target.responseText);
+//     options.cohorts = dataCohorts;
+
+//     //console.log(dataCohorts);
+//     // options.cohort = dataCohorts;
+//     // console.log(options);
+//     mostrarCohorts(dataCohorts);
+//   };
+//   getData(urlCohorts, llamadoCohorts)
+// };
+// getData(urlProgress, llamadoProgress);
+// getData(urlUsers, llamadoUser);
+
+
+// //SELECCIONAR SEDE, CREAR OPTIONS EN SEDES Y MOSTRAR COHORTS 
+// const mostrarCohorts = (cohorts) => {
+//   sedes.addEventListener('change', (event) => {
+//     //console.log(event.target.value)
+//     cohortSelect.innerHTML = ''
+//     cohorts.forEach(elementCohort => {
+//       if (elementCohort.id.startsWith(event.target.value)) {
+//         //console.log(elementCohort.id);
+//         cohortSelect.innerHTML += `<option value="${elementCohort.id}">${elementCohort.id}</option>`;
+//       }
+//     });
+//   });
+// }
+
+// //LLAMANDO AL BUSCADOR
+// searchStudent.addEventListener('keyup', function () {
+//   options.search = searchStudent.value;
+//   let data = processCohortData(options);
+//   console.log(data);
+
+//   addUsers.innerHTML = '';
+//   // console.log(showUser(data));
+//   viewUser(data);
+//   // console.log(showUser(data));
+
+// })
+
+// //LLAMANDO A SORT
+// orderButton.addEventListener('click', (event) => {
+//   // console.log(event.target.textContent);
+//   // console.log(selectOrderBy.value);
+//   options.orderBy = selectOrderBy.value;
+//   options.orderDirection = selectOrderTo.value;
+//   const newOrder = processCohortData(options);
+//   addUsers.innerHTML = '';
+//   viewUser(newOrder);
+// })
